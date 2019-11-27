66b9dedb1ea2c073f848de30939035ca9ff93732
Nick Ioannidis <nicholasioannidis98@gmail.com>
2019-11-26 19:50:25 -0800

    inner loop naviation

--- robot.py
+++ robot.py
@@ -33,6 +33,7 @@ import string
 
 class Robot:
     def __init__(self):
+        self.inner_position = 0
         self.position = 0
         self.view = []
         rospy.Subscriber("R1/pi_camera/image_raw", Image, self.__getImage)
@@ -60,7 +61,7 @@ class Robot:
         self.__vel.publish(vel_msg)
 
     def imageSliceVer(self):
-        img = self.view[:, 700:701]
+        img = self.view[:, 640:641]
         threshold = 128
         cv_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, cv_bin = cv2.threshold(cv_grey, threshold, 255, cv2.THRESH_BINARY)
@@ -72,6 +73,3 @@ class Robot:
         cv_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, cv_bin = cv2.threshold(cv_grey, threshold, 255, cv2.THRESH_BINARY)
         return cv_bin[0, :]
-    
-    def updatePosition(self, position):
-        self.position = position % 16
